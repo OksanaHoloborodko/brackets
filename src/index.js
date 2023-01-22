ba31f4471c24109c0380a806bf4b872fc3eeac7e
Oksana <holoborodko.oksana@gmail.com>
2023-01-22 16:45:42 +0200

    faet: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let pair = {};
+  let stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    pair[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+    let lastElement = stack[stack.length - 1];
+    
+    if (currentBracket != pair[lastElement]) {
+      stack.push(currentBracket);
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+};
